c0c065ed6fd2bc05ecd4d64d754bf48b0c1b07b6
TheVeryDarkness <3266343194@qq.com>
2024-03-14 09:46:37 +0800

    Support cargo workspace.

--- src/bin/cargo-rudra.rs
+++ src/bin/cargo-rudra.rs
@@ -3,12 +3,14 @@
 #[macro_use]
 extern crate log as log_crate;
 
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fmt::Display;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::time::Duration;
 
+use cargo_metadata::{DependencyKind, Metadata, PackageId};
 use rustc_version::VersionMeta;
 
 use wait_timeout::ChildExt;
@@ -85,19 +87,20 @@ where
 
         // Strip leading `name`.
         let suffix = &arg[name.len()..];
-        let value = if suffix.is_empty() {
-            // This argument is exactly `name`; the next one is the value.
-            match args.next() {
-                Some(arg) => arg,
-                None => return false,
-            }
-        } else if suffix.starts_with('=') {
-            // This argument is `name=value`; get the value.
-            // Strip leading `=`.
-            suffix[1..].to_owned()
-        } else {
-            return false;
-        };
+        let value =
+            if suffix.is_empty() {
+                // This argument is exactly `name`; the next one is the value.
+                match args.next() {
+                    Some(arg) => arg,
+                    None => return false,
+                }
+            } else if suffix.starts_with('=') {
+                // This argument is `name=value`; get the value.
+                // Strip leading `=`.
+                suffix[1..].to_owned()
+            } else {
+                return false;
+            };
 
         if check(&value) {
             return true;
@@ -117,7 +120,58 @@ fn version_info() -> VersionMeta {
         .expect("failed to determine underlying rustc version of Rudra")
 }
 
-fn cargo_package() -> cargo_metadata::Package {
+/// Topologically sorts the packages in the workspace, so that dependencies are built before dependents.
+fn cargo_workspace(metadata: &Metadata) -> Vec<cargo_metadata::Package> {
+    let mut dependencies = HashMap::<PackageId, HashSet<PackageId>>::new();
+    let nodes = &metadata
+        .resolve
+        .as_ref()
+        .expect("Can't resolve metadata.")
+        .nodes;
+    for package in nodes {
+        dependencies.insert(
+            package.id.clone(),
+            package
+                .deps
+                .iter()
+                .filter(|dep| {
+                    dep.dep_kinds
+                        .iter()
+                        .any(|kind| kind.kind == DependencyKind::Normal)
+                })
+                .map(|dep| &dep.pkg)
+                .cloned()
+                .collect(),
+        );
+    }
+    let n = dependencies.len();
+    let mut res = vec![];
+    for _ in 0..n {
+        // eprintln!("degrees: {:?}", degrees);
+        let package_id = dependencies
+            .iter()
+            .find(|(_, deps)| deps.is_empty())
+            .map(|(id, _)| id.clone())
+            .unwrap_or_else(|| panic!("Cyclic dependencies in workspace. {:#?}", dependencies));
+        res.push(package_id.clone());
+        dependencies.remove(&package_id);
+        // eprintln!("{}", package_id);
+        for (_, deps) in &mut dependencies {
+            deps.remove(&package_id);
+        }
+    }
+    let workspace = metadata.workspace_members.iter().collect::<HashSet<_>>();
+    res.into_iter()
+        .filter(|pkg| workspace.contains(pkg))
+        .map(|id| metadata[&id].clone())
+        .collect()
+}
+
+fn get_manifest_path() -> Option<PathBuf> {
+    get_arg_flag_value("--manifest-path").map(|m| Path::new(&m).to_path_buf())
+}
+
+fn get_meta() -> Metadata {
     // We need to get the manifest, and then the metadata, to enumerate targets.
     let manifest_path =
         get_arg_flag_value("--manifest-path").map(|m| Path::new(&m).canonicalize().unwrap());
@@ -126,35 +180,16 @@ fn cargo_package() -> cargo_metadata::Package {
     if let Some(manifest_path) = &manifest_path {
         cmd.manifest_path(manifest_path);
     }
-    let mut metadata = match cmd.exec() {
+    match cmd.exec() {
         Ok(metadata) => metadata,
         Err(e) => show_error(format!("Could not obtain Cargo metadata\n{}", e)),
-    };
+    }
+}
 
-    let current_dir = std::env::current_dir();
-
-    let package_index = metadata
-        .packages
-        .iter()
-        .position(|package| {
-            let package_manifest_path = Path::new(&package.manifest_path);
-            if let Some(manifest_path) = &manifest_path {
-                package_manifest_path == manifest_path
-            } else {
-                let current_dir = current_dir
-                    .as_ref()
-                    .expect("could not read current directory");
-                let package_manifest_directory = package_manifest_path
-                    .parent()
-                    .expect("could not find parent directory of package manifest");
-                package_manifest_directory == current_dir
-            }
-        })
-        .unwrap_or_else(|| {
-            show_error("This seems to be a workspace, which is not supported by cargo-rudra");
-        });
-
-    metadata.packages.remove(package_index)
+/// Topologically sorts the packages in the workspace, so that dependencies are built before dependents.
+fn cargo_package() -> Vec<cargo_metadata::Package> {
+    let metadata = get_meta();
+    cargo_workspace(&metadata)
 }
 
 /// Returns the path to the `rudra` binary
@@ -173,7 +208,7 @@ fn test_sysroot_consistency() {
             .arg("--print")
             .arg("sysroot")
             .output()
-            .expect("Failed to run rustc to get sysroot info");
+            .expect(&format!("Failed to run {:?} to get sysroot info", cmd));
         let stdout = String::from_utf8(out.stdout).expect("stdout is not valid UTF-8");
         let stderr = String::from_utf8(out.stderr).expect("stderr is not valid UTF-8");
         let stdout = stdout.trim();
@@ -203,21 +238,25 @@ fn test_sysroot_consistency() {
     }
 }
 
-fn clean_package(package_name: &str) {
+fn clean_package(package_name: &str, manifest_path: Option<&PathBuf>) {
     let mut cmd = Command::new("cargo");
     cmd.arg("clean");
 
     cmd.arg("-p");
     cmd.arg(package_name);
 
+    if let Some(manifest_path) = manifest_path {
+        cmd.arg("--manifest-path").arg(manifest_path);
+    }
+
     cmd.arg("--target");
     cmd.arg(version_info().host);
 
-    let exit_status = cmd
-        .spawn()
-        .expect("could not run cargo clean")
-        .wait()
-        .expect("failed to wait for cargo?");
+    let exit_status =
+        cmd.spawn()
+            .expect("could not run cargo clean")
+            .wait()
+            .expect("failed to wait for cargo?");
 
     if !exit_status.success() {
         show_error(format!("cargo clean failed"));
@@ -233,14 +272,24 @@ fn main() {
 
     log::setup_logging(Verbosity::Normal).expect("Rudra failed to initialize");
 
-    if let Some("rudra") = std::env::args().nth(1).as_ref().map(AsRef::as_ref) {
+    if std::env::args()
+        .nth(1)
+        .as_ref()
+        .map(|s| s.ends_with("rudra"))
+        .unwrap_or(false)
+    {
         progress_info!("Running cargo rudra");
         // This arm is for when `cargo rudra` is called. We call `cargo rustc` for each applicable target,
         // but with the `RUSTC` env var set to the `cargo-rudra` binary so that we come back in the other branch,
         // and dispatch the invocations to `rustc` and `rudra`, respectively.
         in_cargo_rudra();
         progress_info!("cargo rudra finished");
-    } else if let Some("rustc") = std::env::args().nth(1).as_ref().map(AsRef::as_ref) {
+    } else if std::env::args()
+        .nth(1)
+        .as_ref()
+        .map(|s| s.ends_with("rustc"))
+        .unwrap_or(false)
+    {
         // This arm is executed when `cargo-rudra` runs `cargo rustc` with the `RUSTC_WRAPPER` env var set to itself:
         // dependencies get dispatched to `rustc`, the final test/binary to `rudra`.
         inside_cargo_rustc();
@@ -297,122 +346,130 @@ fn in_cargo_rudra() {
     test_sysroot_consistency();
 
     // Now run the command.
-    let package = cargo_package();
-    let mut targets: Vec<_> = package.targets.into_iter().collect();
+    let packages = cargo_package();
 
-    // Ensure `lib` is compiled before `bin`
-    targets.sort_by_key(|target| TargetKind::from(target) as u8);
+    let manifest_path = get_manifest_path();
 
-    for target in targets {
-        // Skip `cargo rudra`
-        let mut args = std::env::args().skip(2);
-        let kind = TargetKind::from(&target);
+    for package in &packages {
+        let mut targets = package.targets.clone();
 
-        // Now we run `cargo check $FLAGS $ARGS`, giving the user the
-        // change to add additional arguments. `FLAGS` is set to identify
-        // this target. The user gets to control what gets actually passed to Rudra.
-        let mut cmd = Command::new("cargo");
-        cmd.arg("check");
+        // Ensure `lib` is compiled before `bin`
+        targets.sort_by_key(|target| TargetKind::from(target) as u8);
 
-        // Allow an option to use `xargo check` instead of `cargo`, this is used
-        // for analyzing the rust standard library.
-        if std::env::var_os("RUDRA_USE_XARGO_INSTEAD_OF_CARGO").is_some() {
-            cmd = Command::new("xargo-check");
-        }
+        for target in targets {
+            // Skip `cargo rudra`
+            let mut args = std::env::args().skip(2);
+            let kind = TargetKind::from(&target);
 
-        match kind {
-            TargetKind::Bin => {
-                // Analyze all the binaries.
-                cmd.arg("--bin").arg(&target.name);
+            // Now we run `cargo check $FLAGS $ARGS`, giving the user the
+            // change to add additional arguments. `FLAGS` is set to identify
+            // this target. The user gets to control what gets actually passed to Rudra.
+            let mut cmd = Command::new("cargo");
+            cmd.arg("check");
+
+            cmd.arg("-p")
+                .arg(format!("{}@{}", package.name, package.version));
+
+            // Allow an option to use `xargo check` instead of `cargo`, this is used
+            // for analyzing the rust standard library.
+            if std::env::var_os("RUDRA_USE_XARGO_INSTEAD_OF_CARGO").is_some() {
+                cmd = Command::new("xargo-check");
             }
-            TargetKind::Library => {
-                // There can be only one lib in a crate.
-                cmd.arg("--lib");
-                // Clean the result to disable Cargo's freshness check
-                clean_package(&package.name);
-            }
-            TargetKind::Unknown => {
-                warn!(
-                    "Target {}:{} is not supported",
-                    target.kind.as_slice().join("/"),
-                    &target.name
-                );
-                continue;
-            }
-        }
 
-        if !cfg!(debug_assertions) && !verbose {
-            cmd.arg("-q");
-        }
-
-        // Forward user-defined `cargo` args until first `--`.
-        while let Some(arg) = args.next() {
-            if arg == "--" {
-                break;
-            }
-            cmd.arg(arg);
-        }
-
-        // We want to always run `cargo` with `--target`. This later helps us detect
-        // which crates are proc-macro/build-script (host crates) and which crates are
-        // needed for the program itself.
-        if get_arg_flag_value("--target").is_none() {
-            // When no `--target` is given, default to the host.
-            cmd.arg("--target");
-            cmd.arg(version_info().host);
-        }
-
-        // Add suffix to RUDRA_REPORT_PATH
-        if let Ok(report) = env::var("RUDRA_REPORT_PATH") {
-            cmd.env(
-                "RUDRA_REPORT_PATH",
-                format!("{}-{}-{}", report, kind, &target.name),
-            );
-        }
-
-        // Serialize the remaining args into a special environment variable.
-        // This will be read by `inside_cargo_rustc` when we go to invoke
-        // our actual target crate (the binary or the test we are running).
-        // Since we're using "cargo check", we have no other way of passing
-        // these arguments.
-        let args_vec: Vec<String> = args.collect();
-        cmd.env(
-            "RUDRA_ARGS",
-            serde_json::to_string(&args_vec).expect("failed to serialize args"),
-        );
-
-        // Set `RUSTC_WRAPPER` to ourselves.  Cargo will prepend that binary to its usual invocation,
-        // i.e., the first argument is `rustc` -- which is what we use in `main` to distinguish
-        // the two codepaths.
-        if env::var_os("RUSTC_WRAPPER").is_some() {
-            println!("WARNING: Ignoring existing `RUSTC_WRAPPER` environment variable, Rudra does not support wrapping.");
-        }
-
-        let path = std::env::current_exe().expect("current executable path invalid");
-        cmd.env("RUSTC_WRAPPER", path);
-        if verbose {
-            cmd.env("RUDRA_VERBOSE", ""); // this makes `inside_cargo_rustc` verbose.
-            eprintln!("+ {:?}", cmd);
-        }
-
-        progress_info!("Running rudra for target {}:{}", kind, &target.name);
-        let mut child = cmd.spawn().expect("could not run cargo check");
-        // 1 hour timeout
-        match child
-            .wait_timeout(Duration::from_secs(60 * 60))
-            .expect("failed to wait for subprocess")
-        {
-            Some(exit_status) => {
-                if !exit_status.success() {
-                    show_error("Finished with non-zero exit code");
+            match kind {
+                TargetKind::Bin => {
+                    // Analyze all the binaries.
+                    cmd.arg("--bin").arg(&target.name);
+                }
+                TargetKind::Library => {
+                    // There can be only one lib in a crate.
+                    cmd.arg("--lib");
+                    // Clean the result to disable Cargo's freshness check
+                    clean_package(&package.name, manifest_path.as_ref());
+                }
+                TargetKind::Unknown => {
+                    warn!(
+                        "Target {}:{} is not supported",
+                        target.kind.as_slice().join("/"),
+                        &target.name
+                    );
+                    continue;
                 }
             }
-            None => {
-                child.kill().expect("failed to kill subprocess");
-                child.wait().expect("failed to wait for subprocess");
-                show_error("Killed due to timeout");
+
+            if !cfg!(debug_assertions) && !verbose {
+                cmd.arg("-q");
             }
-        };
+
+            // Forward user-defined `cargo` args until first `--`.
+            while let Some(arg) = args.next() {
+                if arg == "--" {
+                    break;
+                }
+                cmd.arg(arg);
+            }
+
+            // We want to always run `cargo` with `--target`. This later helps us detect
+            // which crates are proc-macro/build-script (host crates) and which crates are
+            // needed for the program itself.
+            if get_arg_flag_value("--target").is_none() {
+                // When no `--target` is given, default to the host.
+                cmd.arg("--target");
+                cmd.arg(version_info().host);
+            }
+
+            // Add suffix to RUDRA_REPORT_PATH
+            if let Ok(report) = env::var("RUDRA_REPORT_PATH") {
+                cmd.env(
+                    "RUDRA_REPORT_PATH",
+                    format!("{}-{}-{}", report, kind, &target.name),
+                );
+            }
+
+            // Serialize the remaining args into a special environment variable.
+            // This will be read by `inside_cargo_rustc` when we go to invoke
+            // our actual target crate (the binary or the test we are running).
+            // Since we're using "cargo check", we have no other way of passing
+            // these arguments.
+            let args_vec: Vec<String> = args.collect();
+            cmd.env(
+                "RUDRA_ARGS",
+                serde_json::to_string(&args_vec).expect("failed to serialize args"),
+            );
+
+            // Set `RUSTC_WRAPPER` to ourselves.  Cargo will prepend that binary to its usual invocation,
+            // i.e., the first argument is `rustc` -- which is what we use in `main` to distinguish
+            // the two codepaths.
+            if env::var_os("RUSTC_WRAPPER").is_some() {
+                println!("WARNING: Ignoring existing `RUSTC_WRAPPER` environment variable, Rudra does not support wrapping.");
+            }
+
+            let path = std::env::current_exe().expect("current executable path invalid");
+            cmd.env("RUSTC_WRAPPER", path);
+            if verbose {
+                cmd.env("RUDRA_VERBOSE", ""); // this makes `inside_cargo_rustc` verbose.
+                eprintln!("+ {:?}", cmd);
+            }
+
+            progress_info!("Running rudra for target {}:{}", kind, &target.name);
+            let mut child = cmd.spawn().expect("could not run cargo check");
+            // 1 hour timeout
+            match child
+                .wait_timeout(Duration::from_secs(60 * 60))
+                .expect("failed to wait for subprocess")
+            {
+                Some(exit_status) => {
+                    if !exit_status.success() {
+                        show_error("Finished with non-zero exit code");
+                    }
+                }
+                None => {
+                    child.kill().expect("failed to kill subprocess");
+                    child.wait().expect("failed to wait for subprocess");
+                    show_error("Killed due to timeout");
+                }
+            };
+        }
     }
 }
 
